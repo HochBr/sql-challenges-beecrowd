9b60dd3f510fad5117f87c1d964e88c18607b6b7
Hoch <114600733+HochBr@users.noreply.github.com>
2025-01-25 22:50:42 -0300

    Create 3001.sql
    Beecrowd SQL challenge 3001.

---
+++ 3001.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.name,
+    CASE
+        WHEN p.type = 'A' THEN 20.0
+        WHEN p.type = 'B' THEN 70.0
+        WHEN p.type = 'C' THEN 530.5
+    END AS price
+FROM products p
+ORDER BY p.type, p.id DESC;

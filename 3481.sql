672063d6f43b5c8788c44c5dfcb379fa7f55a340
Hoch <114600733+HochBr@users.noreply.github.com>
2025-01-31 20:53:15 -0300

    Create 3481.sql

---
+++ 3481.sql
@@ -0,0 +1,54 @@
+CREATE TABLE nodes (
+    node_id numeric,
+    pointer numeric
+);
+
+insert into nodes (node_id, pointer) values
+(50, 30),
+(50, 75),
+(30, 15),
+(30, 35),
+(15, 1),
+(15, 20),
+(35, 32),
+(35, 40),
+(1, null),
+(20, null),
+(32, null),
+(40, null),
+(75, 60),
+(75, 90),
+(60, 55),
+(60, 70),
+(90, 80),
+(90, 95),
+(55, null),
+(70, null),
+(80, null),
+(95, null);
+
+/*  Execute this query to drop the tables */
+-- DROP TABLE nodes;
+
+
+SELECT DISTINCT 
+    node_id,
+    CASE
+        WHEN pointer IS NULL THEN 'LEAF'
+        WHEN node_id NOT IN (SELECT pointer FROM nodes WHERE pointer IS NOT NULL) THEN 'ROOT'
+        ELSE 'INNER'
+    END AS type
+FROM nodes
+ORDER BY node_id;
+
+
+
+
+
+
+
+
+
+
+
+

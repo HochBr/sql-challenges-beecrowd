fb37b20df65d845c9157d11da45860e194061e7e
Hoch <114600733+HochBr@users.noreply.github.com>
2025-02-08 18:23:10 -0300

    Update 2740.sql

--- 2740.sql
+++ 2740.sql
@@ -30,28 +30,33 @@ VALUES (1, 'The Quack Bats'),
   /*  Execute this query to drop the tables */
   -- DROP TABLE league; --
 
-
-WITH cte_primeiros AS (
-    SELECT 'Podium: ' || team AS name
+-- Básicamente os CTE criados são tabelas temporarias com os dados já "formatados"
+-- WITH nome_da_cte AS(query)
+WITH cte_primeiros AS ( -- Como a tabela já está ordenada então é só pego os 3 primeiros
+    SELECT 'Podium: ' || team AS name -- Os || concatenam os dados
     FROM league
     LIMIT 3
-), 
+), -- caso for usar mais de uma cte é só utilizar uma vírgula :)
+  
 cte_ultimos AS (
     SELECT 'Demoted: ' || team AS name
     FROM league
-    ORDER BY position DESC
+    ORDER BY position DESC 
     LIMIT 1
 ),
+  
 cte_penultimo AS (
     SELECT 'Demoted: ' || team AS name
     FROM league
     WHERE position = (SELECT MAX(position) 
                       FROM league 
-                      WHERE position < (SELECT MAX(position) FROM league))
+                      WHERE position < ( -- Com o uso do MAX a subquary(1) busca o maior valor que é menor que o valor obtido na subquary(2)
+                                        SELECT MAX(position) FROM league) -- Está subquery(2) básicamente busca o maior valor 
+                                       ) 
 )
 
 SELECT * FROM cte_primeiros
-UNION ALL
+UNION ALL -- O UNION ALL de forma bem abstrata funciona como um concatenador de queries
 SELECT * FROM cte_penultimo
 UNION ALL
 SELECT * FROM cte_ultimos;
